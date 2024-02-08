75073da9a8c8f01b0c2a8024414d35a39ae2f3ce
unknown <aguit.inan@gmail.com>
2024-02-08 23:10:56 +0100

    feat: update Exercice04

---
+++ Exercice04/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Exercice04.Models;
+
+public interface IRepository<TEntity>
+{
+    bool Add(TEntity item);
+    TEntity? Get(Expression<Func<TEntity, bool>> predicate);
+    List<TEntity> GetAll();
+    List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+    TEntity? GetById(int id);
+    bool Update(TEntity item);
+    bool Delete(int id);
+
+}
