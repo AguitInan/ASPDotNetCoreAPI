46324fae86b68b67f65a47d23fca84669b2c0ec4
unknown <aguit.inan@gmail.com>
2024-02-08 23:11:58 +0100

    feat: update Exercice04

---
+++ Exercice04/Repositories/ContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using Exercice04.Data;
+using System.Linq.Expressions;
+using Exercice04.Data;
+using Exercice04.Models;
+
+namespace Exercice04.Repositories
+{
+    public class ContactRepository : IRepository<Contact>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ContactRepository(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+    }
+}
