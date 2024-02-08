691050ba1e75981e83e0e4e079eefc13e8c2173b
unknown <aguit.inan@gmail.com>
2024-02-08 23:14:10 +0100

    feat: update Exercice04

---
+++ Exercice04/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Exercice04.Models;
+using System.Linq.Expressions;
+using System;
+
+namespace Exercice04.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IRepository<Contact> _contactRepository;
+
+        public ContactsController(IRepository<Contact> contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+    }
+}
