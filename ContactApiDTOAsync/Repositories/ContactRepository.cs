69be0aca740a1f999bd0c4445c902c4a91c9e745
unknown <aguit.inan@gmail.com>
2024-02-09 23:50:14 +0100

    feat: update ContactRepository.cs

--- ContactApiDTOAsync/Repositories/ContactRepository.cs
+++ ContactApiDTOAsync/Repositories/ContactRepository.cs
@@ -13,5 +13,17 @@ namespace ContactApiDTO.Repositories
         {
             _db = db;
         }
+
+        // CREATE
+        public async Task<Contact?> Add(Contact contact)
+        {
+            var addEntry = await _db.Contacts.AddAsync(contact); // retourne un EntityEntry<Contact> qui enveloppe le nouveau contact créé
+            await _db.SaveChangesAsync();
+
+            if (addEntry.Entity.Id > 0) // si l'entité est bien ajoutée l'id est > 0
+                    return addEntry.Entity;
+
+            return null; // erreur lors de l'ajout
+        }
     }
 }
