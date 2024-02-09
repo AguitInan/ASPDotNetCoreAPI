a7129ed2d3ec72a2b597a55cbd8f29081d7b991b
unknown <aguit.inan@gmail.com>
2024-02-09 23:48:06 +0100

    feat: add IRepository.cs

---
+++ ContactApiDTOAsync/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace ContactApiDTO.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        // CREATE
+        //bool Add(TEntity entity); // bool => si ça s'est bien passé
+        //int Add(TEntity entity); // int => id de l'entité
+        Task<TEntity?> Add(TEntity entity); // TEntity => on retourne l'entité qui vient d'être ajoutée
+
+        // READ
+        Task<TEntity?> Get(int id); // uniquement l'id
+        Task<TEntity?> Get(Expression<Func<TEntity, bool>> predicate); // avec un délégué/fonction pour filtrer
+        Task<IEnumerable<TEntity>> GetAll(); // toutes les entités
+        Task<IEnumerable<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate); // avec un délégué/fonction pour filtrer
+
+        // UPDATE
+        //bool Update(TEntity contact); // bool => si ça s'est bien passé
+        Task<TEntity?> Update(TEntity entity); // TEntity => on retourne l'entité qui vient d'être modifiée
+
+        // DELETE
+        Task<bool> Delete(int id);
+
+    }
+}
