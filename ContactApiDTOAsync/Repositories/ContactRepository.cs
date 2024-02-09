87d1fe8b462de28966c6b168570bdde6f87baa2c
unknown <aguit.inan@gmail.com>
2024-02-09 23:49:41 +0100

    feat: add ContactRepository.cs

---
+++ ContactApiDTOAsync/Repositories/ContactRepository.cs
@@ -0,0 +1,17 @@
+﻿using ContactApiDTO.Data;
+using ContactApiDTO.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ContactApiDTO.Repositories
+{
+    public class ContactRepository : IRepository<Contact>
+    {
+        private readonly AppDbContext _db;
+
+        public ContactRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
