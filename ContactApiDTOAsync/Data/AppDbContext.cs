aead1f6df437739a78135ae2187976cca700e4b2
unknown <aguit.inan@gmail.com>
2024-02-09 23:45:18 +0100

    feat: add AppDbContext.cs

---
+++ ContactApiDTOAsync/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using ContactApiDTO.Models;
+using ContactApiDTOAsync.Helpers;
+using ContactApiDTOAsync.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactApiDTO.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+#nullable disable
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            var adminRoot = new User()
+            {
+                Id = 1,
+                FirstName = "Root",
+                LastName = "ROOT",
+                BirthDate = new DateTime(2000, 1, 1),
+                Gender = "X",
+                IsAdmin = true,
+                Email = "root@utopios.com",
+                Password = PasswordCrypter.Encrypt("PAss00++", "Des paillettes dans mes yeux Kevin") 
+                //Password = "UEFzczAwKytEZXMgcGFpbGxldHRlcyBkYW5zIG1lcyB5ZXV4IEtldmlu"
+
+            };
+            modelBuilder.Entity<User>().HasData(adminRoot);
+        }
+    }
+}
