7ca080a57cbcd101ac3ee58f8882c9f1403e5759
unknown <aguit.inan@gmail.com>
2024-02-09 23:42:40 +0100

    feat: add Contact.cs

---
+++ ContactApiDTOAsync/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ContactApiDTO.Models
+{
+    [Table("contact")]
+    public class Contact
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("firstname")]
+        [Required]
+        public string? FirstName { get; set; }
+        [Column("lastname")]
+        [Required]
+        public string? LastName { get; set; }
+        [Column("birth_date")]
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Column("gender")]
+        [Required]
+        [StringLength(1)]
+        public string? Gender { get; set; }
+    }
+}
