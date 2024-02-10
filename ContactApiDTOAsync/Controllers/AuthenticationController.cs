8d6b99f182b6eae7db8907df08d81ebaca08252b
unknown <aguit.inan@gmail.com>
2024-02-10 21:27:29 +0100

    feat: update AuthenticationController.cs

---
+++ ContactApiDTOAsync/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using ContactApiDTO.Data;
+using ContactApiDTOAsync.DTOs;
+using ContactApiDTOAsync.Helpers;
+using ContactApiDTOAsync.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ContactApiDTOAsync.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly AppDbContext _appDbContext1;
+        private AppSettings _appSettings1;
+
+
+        public AuthenticationController(AppDbContext appDbContext, IOptions<AppSettings> appSettings)
+        {
+
+            _appDbContext1 = appDbContext;
+            _appSettings1 = appSettings.Value;
+
+        }
+    }
+}
