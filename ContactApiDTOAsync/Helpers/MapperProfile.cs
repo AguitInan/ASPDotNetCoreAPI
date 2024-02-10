f6bac854280b6ecfb8ce3d1aba1d2f85e4c67f41
unknown <aguit.inan@gmail.com>
2024-02-10 21:18:33 +0100

    feat: add MapperProfile.cs

---
+++ ContactApiDTOAsync/Helpers/MapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ContactApiDTO.DTOs;
+using ContactApiDTO.Models;
+
+namespace ContactApiDTO.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Contact, ContactDTO>().ReverseMap();
+            // cette ligne permet de dire qu'a l'aide du mapper on pourra passer de l'entité vers le DTO
+            // et vice versa grace au .ReverseMap()
+
+            CreateMap<ContactDTO, ContactFullNameDTO>();
+        }
+    }
+}
