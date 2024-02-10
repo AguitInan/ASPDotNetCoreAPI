11c344f1a9229491bf10f0f0171e4725b1c4543b
unknown <aguit.inan@gmail.com>
2024-02-10 21:18:59 +0100

    feat: add Constants.cs

---
+++ ContactApiDTOAsync/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactApiDTOAsync.Helpers
+{
+    public class Constants
+    {
+        public const string PolicyAdmin = "PolicyAdmin";
+        public const string PolicyUser = "PolicyUser";
+        public const string RoleAdmin = "Admin";
+        public const string RoleUser = "User";
+
+    }
+}
