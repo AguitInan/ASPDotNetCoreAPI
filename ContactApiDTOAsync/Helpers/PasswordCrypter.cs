bf50d19839f601afb3fb11ad1657bbef2ccc6ba9
unknown <aguit.inan@gmail.com>
2024-02-10 21:18:04 +0100

    feat: add PasswordCrypter.cs

---
+++ ContactApiDTOAsync/Helpers/PasswordCrypter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace ContactApiDTOAsync.Helpers
+{
+    public class PasswordCrypter
+    {
+
+        public static string Encrypt(string password, string key)
+        {
+            if (string.IsNullOrEmpty(password)) return "";
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(password + key));
+        }
+
+    }
+}
