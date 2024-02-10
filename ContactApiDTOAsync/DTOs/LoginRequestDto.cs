2794f7ae2993159754ee011987f9391c9c3f0c8d
unknown <aguit.inan@gmail.com>
2024-02-10 21:21:33 +0100

    feat: add LoginRequestDto.cs

---
+++ ContactApiDTOAsync/DTOs/LoginRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using ContactApiDTOAsync.Validator;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactApiDTOAsync.DTOs
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        [PasswordValidator]
+        public string Password { get; set; }
+
+
+    }
+}
