8422a5f2d2762e99a234ab8956fb0b5d6c6aa70e
unknown <aguit.inan@gmail.com>
2024-02-10 21:19:27 +0100

    feat: add AppSettings.cs

---
+++ ContactApiDTOAsync/Helpers/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactApiDTOAsync.Helpers
+{
+    public class AppSettings
+    {
+
+        public string? SecretKey { get; set; }
+        public string? ValidAudience { get; set; }
+        public string? ValidIssuer { get; set; }
+
+
+
+    }
+}
