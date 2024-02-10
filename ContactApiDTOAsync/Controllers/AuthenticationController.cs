07f305759f652670bde345a777a09811835a2e82
unknown <aguit.inan@gmail.com>
2024-02-10 21:28:02 +0100

    feat: update AuthenticationController.cs

--- ContactApiDTOAsync/Controllers/AuthenticationController.cs
+++ ContactApiDTOAsync/Controllers/AuthenticationController.cs
@@ -46,5 +46,46 @@ namespace ContactApiDTOAsync.Controllers
             return BadRequest("Probleme creation User");
 
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto login)
+        {
+            login.Password = PasswordCrypter.Encrypt(login.Password, _appSettings1.SecretKey!);
+
+            var user = await _appDbContext1.Users.FirstOrDefaultAsync(u => u.Email == login.Email && u.Password == login.Password);
+
+            if (user == null) return BadRequest("Invalid Authentication !");
+
+            var role = user.IsAdmin ? Constants.RoleAdmin : Constants.RoleUser;
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Role, role),
+                new Claim("Userid",user.Id.ToString())
+
+            };
+
+
+            SigningCredentials signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings1.SecretKey!)), SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken jwt = new JwtSecurityToken(
+                claims: claims,
+                issuer: _appSettings1.ValidIssuer,
+                audience: _appSettings1.ValidAudience,
+                signingCredentials: signingCredentials,
+                expires: DateTime.Now.AddHours(2));
+
+            string token = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return Ok(new
+            {
+                Token = token,
+                Message = "Authentication Succefull !!",
+                User = user
+            });
+
+
+
+        }
     }
 }
