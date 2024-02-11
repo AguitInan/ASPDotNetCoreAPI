ca625fef9877512476ed36e83217acbb0a9d37d5
unknown <aguit.inan@gmail.com>
2024-02-11 13:13:12 +0100

    feat: add LoginRequestDto.cs

---
+++ Exercice05/DTOs/LoginRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using Exercice05.Validator;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exercice05.DTOs
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        [PasswordValidator]
+        public string Password { get; set; }
+
+
+    }
+}
