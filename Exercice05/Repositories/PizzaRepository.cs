9a49286371550dce3605ce6340a01e35cb8e58c4
unknown <aguit.inan@gmail.com>
2024-02-11 13:21:04 +0100

    feat: update PizzaRepository.cs

--- Exercice05/Repositories/PizzaRepository.cs
+++ Exercice05/Repositories/PizzaRepository.cs
@@ -13,5 +13,17 @@ namespace Exercice05.Repositories
         {
             _db = db;
         }
+
+        // CREATE
+        public async Task<Pizza?> Add(Pizza pizza)
+        {
+            var addEntry = await _db.Pizzas.AddAsync(pizza); // retourne un EntityEntry<Contact> qui enveloppe le nouveau contact créé
+            await _db.SaveChangesAsync();
+
+            if (addEntry.Entity.Id > 0) // si l'entité est bien ajoutée l'id est > 0
+                return addEntry.Entity;
+
+            return null; // erreur lors de l'ajout
+        }
     }
 }
