afd9b307ffbd0bf757cd35286558da4c02b651a7
unknown <aguit.inan@gmail.com>
2024-02-11 13:09:21 +0100

    feat: add AppDbContext.cs

---
+++ Exercice05/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Exercice05.Models;
+using Exercice05.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static Exercice05.Models.Pizza;
+
+namespace Exercice05.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+#nullable disable
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
