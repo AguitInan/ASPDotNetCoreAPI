24e040f5a2a3b02fb34b84ba11b8c607cac23994
unknown <aguit.inan@gmail.com>
2024-02-11 13:05:36 +0100

    feat: add Pizza.cs

---
+++ Exercice05/Models/Pizza.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Exercice05.Validator;
+
+namespace Exercice05.Models
+{
+    [Table("pizza")]
+    public class Pizza
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; }
+        [Column("description")]
+        [Required]
+        public string Description { get; set; }
+        [Column("price")]
+        [Required]
+        public decimal Price { get; set; }
+        [Column("image_url")]
+        [Required]
+        public string ImageUrl { get; set; }
+        [Column("pizza_type")]
+        [Required]
+        public PizzaType Variety { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public enum PizzaType
+        {
+            Vegetarienne,
+            Piquante
+        }
+    }
+}
