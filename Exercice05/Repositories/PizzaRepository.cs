b5bc1b8073aecd6c00168f13970848f1b3cc8ff4
unknown <aguit.inan@gmail.com>
2024-02-11 13:20:45 +0100

    feat: add PizzaRepository.cs

---
+++ Exercice05/Repositories/PizzaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Exercice05.Data;
+using Exercice05.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Exercice05.Repositories
+{
+    public class PizzaRepository : IRepository<Pizza>
+    {
+        private readonly AppDbContext _db;
+
+        public PizzaRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
