fa1c123012ecf3625510d53d313e48fe49f211ad
unknown <aguit.inan@gmail.com>
2024-02-11 13:18:43 +0100

    feat: add IngredientRepository.cs

---
+++ Exercice05/Repositories/IngredientRepository.cs
@@ -0,0 +1,17 @@
+﻿using Exercice05.Data;
+using Exercice05.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Exercice05.Repositories
+{
+    public class IngredientRepository : IRepository<Ingredient>
+    {
+        private readonly AppDbContext _db;
+
+        public IngredientRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
