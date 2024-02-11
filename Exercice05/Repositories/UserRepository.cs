1007cfa9703a81e93e351bab57e5aca4826c5daa
unknown <aguit.inan@gmail.com>
2024-02-11 13:17:07 +0100

    feat: update UserRepository.cs

--- Exercice05/Repositories/UserRepository.cs
+++ Exercice05/Repositories/UserRepository.cs
@@ -25,5 +25,18 @@ namespace Exercice05.Repositories
 
             return null; // erreur lors de l'ajout
         }
+
+
+        // READ
+        public async Task<User?> Get(int id)
+        {
+            //return _db.Contacts.Find(id); // ne fonctionne que sur un DbSet<> (EFCore)
+            return await _db.Users.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<User?> Get(Expression<Func<User, bool>> predicate)
+        {
+            return await _db.Users.FirstOrDefaultAsync(predicate);
+        }
     }
 }
