51d1a356fa3d9ca076a456419d1f05204007b3a5
unknown <aguit.inan@gmail.com>
2024-02-11 13:04:41 +0100

    feat: add Ingredient.cs

---
+++ Exercice05/Models/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exercice05.Models
+{
+    [Table("ingredient")]
+    public class Ingredient
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; }
+        [Column("description")]
+        [Required]
+        public string Description { get; set; }
+    }
+}
