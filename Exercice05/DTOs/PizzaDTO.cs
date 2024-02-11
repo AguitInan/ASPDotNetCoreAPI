58feea7dd50fd5937cb9b9000932d3815a5b7724
unknown <aguit.inan@gmail.com>
2024-02-11 13:14:34 +0100

    feat: add PizzaDTO.cs

---
+++ Exercice05/DTOs/PizzaDTO.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Exercice05.Models;
+
+namespace Exercice05.DTOs
+{
+    public class PizzaDTO
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; }
+        [Column("description")]
+        [Required]
+        public string Description { get; set; }
+        [Column("price")]
+        [Required]
+        public decimal Price { get; set; }
+        [Column("image_url")]
+        [Required]
+        public string ImageUrl { get; set; }
+        [Column("pizza_type")]
+        [Required]
+        public PizzaType Variety { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public enum PizzaType
+        {
+            Vegetarienne,
+            Piquante
+        }
+    }
+}
