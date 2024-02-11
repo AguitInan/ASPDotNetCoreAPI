b89a3e7791815867e882262b57d3f68a0cd38362
unknown <aguit.inan@gmail.com>
2024-02-11 13:16:33 +0100

    feat: add UserRepository.cs

---
+++ Exercice05/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Exercice05.Data;
+using Exercice05.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Exercice05.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly AppDbContext _db;
+
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
