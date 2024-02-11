e3e74c7ba61626bd1ddda92b4701616d99799bd8
unknown <aguit.inan@gmail.com>
2024-02-11 13:13:40 +0100

    feat: add IngredientDTO.cs

---
+++ Exercice05/DTOs/IngredientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Exercice05.DTOs
+{
+    public class IngredientDTO
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; }
+        [Column("description")]
+        [Required]
+        public string Description { get; set; }
+
+    }
+}
