95c973b064e01b25b24791f1ca9c2036c2291287
unknown <aguit.inan@gmail.com>
2024-02-12 14:31:23 +0100

    feat: update PizzaController.cs

--- Exercice05/Controllers/PizzaController.cs
+++ Exercice05/Controllers/PizzaController.cs
@@ -26,5 +26,20 @@ namespace Exercice05.Controllers
             _repository = repository;
             _mapper = mapper;
         }
+
+        //GET /contacts
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //return Ok(_repository.GetAll());
+            IEnumerable<Pizza> pizzas = await _repository.GetAll();
+
+            IEnumerable<PizzaDTO> pizzaDTOs = _mapper.Map<IEnumerable<PizzaDTO>>(pizzas)!;
+            //IEnumerable<ContactDTO> contactDTOs = _mapper.Map<IEnumerable<Contact>, IEnumerable<ContactDTO>>(contacts)!;
+
+            // possible d'ajouter des modification par rapport aux DTOs ici
+
+            return Ok(pizzaDTOs);
+        }
     }
 }
