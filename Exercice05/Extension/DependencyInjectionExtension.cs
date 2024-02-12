2a0d8c352eeb82a4503881665aaab3cc78891a79
unknown <aguit.inan@gmail.com>
2024-02-12 09:42:03 +0100

    feat: add DependendyInjectionExtension.cs

---
+++ Exercice05/Extension/DependencyInjectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Exercice05.Data;
+using Exercice05.Models;
+using Exercice05.Repositories;
+using Exercice05.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Security.Claims;
+using System.Text;
+
+namespace Exercice05.Extension
+{
+    public static class DependencyInjectionExtension
+    {
+
+        public static void InjectDependancies(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.addSwagger();
+            builder.AddRepositories();
+            builder.AddDatabase();
+            builder.AddAuthentication();
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
