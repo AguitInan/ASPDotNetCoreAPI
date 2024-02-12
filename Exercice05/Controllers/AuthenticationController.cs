8dd49c1d46ff2774d29cf1a64e2c0f4ee9701284
unknown <aguit.inan@gmail.com>
2024-02-12 14:27:09 +0100

    feat: add AuthenticationController.cs

---
+++ Exercice05/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using Exercice05.Data;
+using Exercice05.DTOs;
+using Exercice05.Helpers;
+using Exercice05.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Exercice05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly AppDbContext _appDbContext1;
+        private AppSettings _appSettings1;
+
+
+        public AuthenticationController(AppDbContext appDbContext, IOptions<AppSettings> appSettings)
+        {
+
+            _appDbContext1 = appDbContext;
+            _appSettings1 = appSettings.Value;
+
+        }
+    }
+}
