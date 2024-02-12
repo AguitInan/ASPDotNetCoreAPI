a3aaa636546b3f9e1f03318efc112aff3d162867
unknown <aguit.inan@gmail.com>
2024-02-12 14:28:49 +0100

    feat: add IngredientController.cs

---
+++ Exercice05/Controllers/IngredientController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Exercice05.DTOs;
+using Exercice05.Models;
+using Exercice05.Repositories;
+using Exercice05.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Exercice05.Controllers
+{
+
+    [Route("ingredients")]
+    [ApiController]
+    [Authorize]
+    public class IngredientController : ControllerBase
+    {
+        private readonly IRepository<Ingredient> _repository;
+        private readonly IMapper _mapper;
+
+        public IngredientController(IRepository<Ingredient> repository,
+                                 IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
