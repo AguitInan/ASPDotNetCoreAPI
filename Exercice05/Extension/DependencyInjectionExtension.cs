bd56c809d48c04a19e2cedb5fcf71cea0b8d6278
unknown <aguit.inan@gmail.com>
2024-02-12 09:43:30 +0100

    feat: update DependendyInjectionExtension.cs

--- Exercice05/Extension/DependencyInjectionExtension.cs
+++ Exercice05/Extension/DependencyInjectionExtension.cs
@@ -78,5 +78,70 @@ namespace Exercice05.Extension
             var conn = builder.Configuration.GetConnectionString("Default");
             builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(conn));
         }
+
+
+
+        public static void AddAuthentication(this WebApplicationBuilder builder)
+        {
+            var appSettingSection = builder.Configuration.GetSection(nameof(AppSettings));
+
+            builder.Services.Configure<AppSettings>(appSettingSection);
+
+            AppSettings appSettings = appSettingSection.Get<AppSettings>();
+
+
+            var key = Encoding.ASCII.GetBytes(appSettings.SecretKey);
+
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateAudience = true,
+                        ValidAudience = appSettings.ValidAudience,
+                        ValidateIssuer = true,
+                        ValidIssuer = appSettings.ValidIssuer,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+
+                    };
+
+                });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy(Constants.PolicyAdmin, policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, Constants.RoleAdmin);
+                    // policy.RequireClaim(ClaimTypes.Role, Constants.RoleUser);
+                });
+
+                options.AddPolicy(Constants.PolicyUser, policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, Constants.RoleUser);
+                }
+               );
+
+
+            });
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
