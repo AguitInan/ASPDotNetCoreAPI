35a60c3bb44d0fc0959123da6fd791e2772a2daf
unknown <aguit.inan@gmail.com>
2024-02-12 09:17:25 +0100

    feat: add MapperProfile.cs

---
+++ Exercice05/Helpers/MapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Exercice05.DTOs;
+using Exercice05.Models;
+
+namespace Exercice05.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            // cette ligne permet de dire qu'a l'aide du mapper on pourra passer de l'entité vers le DTO
+            // et vice versa grace au .ReverseMap()
+
+            CreateMap<Ingredient, IngredientDTO>().ReverseMap();
+            // cette ligne permet de dire qu'a l'aide du mapper on pourra passer de l'entité vers le DTO
+            // et vice versa grace au .ReverseMap()
+
+            CreateMap<Pizza, PizzaDTO>().ReverseMap();
+            // cette ligne permet de dire qu'a l'aide du mapper on pourra passer de l'entité vers le DTO
+            // et vice versa grace au .ReverseMap()
+
+            //CreateMap<ContactDTO, ContactFullNameDTO>();
+        }
+    }
+}
