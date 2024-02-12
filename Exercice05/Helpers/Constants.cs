5d536bf54a454ab7251af770b4b42cf548692293
unknown <aguit.inan@gmail.com>
2024-02-12 09:17:54 +0100

    feat: add Constants.cs

---
+++ Exercice05/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercice05.Helpers
+{
+    public class Constants
+    {
+        public const string PolicyAdmin = "PolicyAdmin";
+        public const string PolicyUser = "PolicyUser";
+        public const string RoleAdmin = "Admin";
+        public const string RoleUser = "User";
+
+    }
+}
