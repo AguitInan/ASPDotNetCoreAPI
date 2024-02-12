e754a70cfc576bfd25fc2b738280a4954570e81f
unknown <aguit.inan@gmail.com>
2024-02-12 14:31:06 +0100

    feat: add PizzaController.cs

---
+++ Exercice05/Controllers/PizzaController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Exercice05.DTOs;
+using Exercice05.Models;
+using Exercice05.Repositories;
+using Exercice05.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Exercice05.Controllers
+{
+
+    [Route("pizzas")]
+    [ApiController]
+    [Authorize]
+    public class PizzaController : ControllerBase
+    {
+        private readonly IRepository<Pizza> _repository;
+        private readonly IMapper _mapper;
+
+        public PizzaController(IRepository<Pizza> repository,
+                                 IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
